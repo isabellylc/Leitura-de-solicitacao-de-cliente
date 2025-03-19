5a07e2c99ef6db0577bf9960da056715b130ced6
isabellylc <santosisabelly771@gmail.com>
2025-03-19 11:00:38 -0300

    update: insere o form4

---
+++ Leitura de solicitação de cliente/Form4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leitura_de_solicitação_de_cliente
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+    }
+}
